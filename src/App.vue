5e4e818ba8a1dcc1ddc451625fb63bd25b298793
tpf1220 <tpf1220@sina.com>
2018-05-07 19:52:37 +0800

    init demo

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less" scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  .header{
+    padding:10px;
+  }
+
+}
+</style>
